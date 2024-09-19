0d30c064c07cf8b263b07813a952fbcdf1d21b9e
iuu666 <okgo.orz@gmail.com>
2024-09-20 00:34:34 +0800

    Update ChinaIP.py

--- scripts/ChinaIP.py
+++ scripts/ChinaIP.py
@@ -4,64 +4,47 @@ import json
 from datetime import datetime, timedelta
 
 # 生成头部信息
-def get_header(file_name):
+def get_header(file_name, count):
     utc_time = datetime.utcnow()  # 获取当前的 UTC 时间
     china_time = utc_time + timedelta(hours=8)  # 转换为 CST (UTC+8)
     local_time = china_time.strftime("%Y-%m-%d %H:%M:%S")  # 格式化时间为字符串
     # 根据文件名生成相应的头部信息
     header = f"""\
-# {file_name.replace('.list', '')} (https://github.com/iuu666/ASN.China)
-# Last Updated: CST {local_time}
-# Made by iuu, All rights reserved.
+// {file_name.replace('.list', '')}
+// Link: https://github.com/iuu666/ASN.China
+// Total Count: {count}
+// Last Updated: CST {local_time}
+// Made by iuu, All rights reserved.
+
 """
     return header
+
+def fetch_and_save(url, file_name):
+    # 发起 GET 请求获取页面内容
+    r = requests.get(url).text
+    # 解析 HTML 内容
+    tree = etree.HTML(r)
+    # 提取嵌入的数据
+    asns = tree.xpath('//*[@data-target="react-app.embeddedData"]')[0].text
+    # 解析 JSON 数据
+    x = json.loads(asns)['payload']['blob']['rawLines']
+    # 计算总记录数
+    count = len(x)
+    # 保存数据到文件
+    with open(file_name, "w", encoding='utf-8') as file:
+        # 写入头部信息
+        file.write(get_header(file_name, count))
+        # 写入数据
+        for i in x:
+            file.write(i)
+            file.write('\n')
 
 # 各种 IP 列表的 URL
 allChina = "https://github.com/cbuijs/ipasn/blob/master/country-asia-china.list"
 v4China = "https://github.com/cbuijs/ipasn/blob/master/country-asia-china4.list"
 v6China = "https://github.com/cbuijs/ipasn/blob/master/country-asia-china6.list"
 
-# 获取所有 IP 地址的列表
-r = requests.get(allChina)
-# 解析 HTML 内容
-tree = etree.HTML(r.text)
-# 提取嵌入的数据
-asns = tree.xpath('//*[@data-target="react-app.embeddedData"]')[0].text
-# 解析 JSON 数据
-x = json.loads(asns)['payload']['blob']['rawLines']
-# 保存所有 IP 地址到文件
-with open("IP.China.list", "w", encoding='utf-8') as allChinaIP:
-    allChinaIP.write(get_header("IP.China.list"))
-    for i in x:
-        allChinaIP.write(i)
-        allChinaIP.write('\n')
-
-# 获取 IPv4 地址列表
-r = requests.get(v4China)
-# 解析 HTML 内容
-tree = etree.HTML(r.text)
-# 提取嵌入的数据
-asns = tree.xpath('//*[@data-target="react-app.embeddedData"]')[0].text
-# 解析 JSON 数据
-x = json.loads(asns)['payload']['blob']['rawLines']
-# 保存 IPv4 地址到文件
-with open("IPv4.China.list", "w", encoding='utf-8') as v4ChinaIP:
-    v4ChinaIP.write(get_header("IPv4.China.list"))
-    for i in x:
-        v4ChinaIP.write(i)
-        v4ChinaIP.write('\n')
-
-# 获取 IPv6 地址列表
-r = requests.get(v6China)
-# 解析 HTML 内容
-tree = etree.HTML(r.text)
-# 提取嵌入的数据
-asns = tree.xpath('//*[@data-target="react-app.embeddedData"]')[0].text
-# 解析 JSON 数据
-x = json.loads(asns)['payload']['blob']['rawLines']
-# 保存 IPv6 地址到文件
-with open("IPv6.China.list", "w", encoding='utf-8') as v6ChinaIP:
-    v6ChinaIP.write(get_header("IPv6.China.list"))
-    for i in x:
-        v6ChinaIP.write(i)
-        v6ChinaIP.write('\n')
+# 执行函数，保存数据
+fetch_and_save(allChina, "IP.China.list")
+fetch_and_save(v4China, "IPv4.China.list")
+fetch_and_save(v6China, "IPv6.China.list")
